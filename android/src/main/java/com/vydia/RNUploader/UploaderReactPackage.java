21c830ebf367d78b4529dc377f15f1a4ced019f0
Kyle Weisel <kyleweisel@gmail.com>
2020-11-18 09:42:38 -0500

    Missing import

--- android/src/main/java/com/vydia/RNUploader/UploaderReactPackage.java
+++ android/src/main/java/com/vydia/RNUploader/UploaderReactPackage.java
@@ -6,6 +6,8 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
+import com.vydia.RNUploader.UploaderModule;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
